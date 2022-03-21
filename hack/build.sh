07761a808a903ab852df37edef79b469ee039567
Raúl Sevilla <rsevilla@redhat.com>
2022-03-21 16:08:43 +0100

    Fix ES module version
    To prevent issues like
    ```
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/fs-drift] RUN STATUS DONE
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Input dictionary is empty for module ocp_default_ingress_controller
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Input dictionary is empty for module ocp_dns
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Input dictionary is empty for module ocp_kube_apiserver
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Input dictionary is empty for module ocp_kube_controllermanager
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Input dictionary is empty for module ocp_network_operator
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Unknown indexing error: The client noticed that the server is not a supported distribution of Elasticsearch
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Indexing exception found The client noticed that the server is not a supported distribution of Elasticsearch
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Closing Redis connection
    [pod/fs-drift-benchmark-client-3f5a0b88-1-5xlqb/backpack] Attempting to close ES connection
    ```
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- hack/build.sh
+++ hack/build.sh
@@ -4,9 +4,11 @@ REGISTRY=${REGISTRY:-quay.io}
 ORG=${ORG:-cloud-bulldozer}
 REPOSITORY=${REPOSITORY:-backpack}
 TAG=${TAG:-latest}
-
 IMAGE=${REGISTRY}/${ORG}/${REPOSITORY}:${TAG}
+ARCHS=linux/amd64,linux/arm64
+
+
 echo "Building backpack into ${IMAGE}"
-podman build --pull-always --no-cache -f Dockerfile -t ${IMAGE}
+podman build --pull-always --jobs=2 --platform=${ARCHS} --manifest ${IMAGE} .
 echo "Pushing backpack to ${IMAGE}"
-podman push ${IMAGE}
+podman manifest push ${IMAGE} ${IMAGE}
